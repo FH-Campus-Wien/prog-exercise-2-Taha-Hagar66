f606ebd7119e2a0379d9c67ffa77af7316dae0c6
taha6 <taha.hagar@stud.fh-campuswien.ac.at>
2022-10-14 23:55:56 +0200

    Tasks completed!

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -1,27 +1,85 @@
 package at.ac.fhcampuswien;
 
 import java.util.Scanner;
-
 public class App {
 
     //todo Task 1
     public void largestNumber(){
-        // input your solution here
+        int count = 1;
+        int i, large, num;
+        float num;
+
+        do {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Number " + count++ + ": ");
+            num = sc.nextInt();
+        }
+        while (num !=0);
+            System.out.println("The largest number is " + num);
+        }
+
+        for(i=0; i<10; i++)
+        arr[i] = scan.nextInt();
+
+        large = arr[0];
+        for(i=1; i<10; i++){
+            if(large<arr[i])
+            large = arr[i];
+        }
+        System.out.println("The largest number is " +large);
     }
 
     //todo Task 2
     public void stairs(){
-        // input your solution here
+        // initialising starting number
+        int i, j, num=1;
+
+        // outer loop to handle number of rows
+        // n in this case
+        for(i=0; i<n; i++)
+        {
+
+            // without re assigning num
+            // num = 1;
+            for(j=0; j<=i; j++)
+            {
+                // printing num with a space
+                System.out.print(num+ " ");
+
+                // incrementing num at each column
+                num = num + 1;
+            }
+
+            // ending line after each row
+            System.out.println();
+        }
     }
 
     //todo Task 3
     public void printPyramid(){
-        // input your solution here
+        int rows = 6, k = 0;
+
+        for (int i = 1; i <= rows; ++i, k = 0) {
+            for (int space = 1; space <= rows - i; ++space) {
+                System.out.print("  ");
+            }
+
+            while (k != 2 * i - 1) {
+                System.out.print("* ");
+                ++k;
+            }
+
+            System.out.println();
+        }
     }
 
     //todo Task 4
     public void printRhombus(){
-        // input your solution here
+        //char c = "H";
+
+        //System.out.println();
+        //System.out.println((int)c);
+
     }
 
     //todo Task 5
@@ -41,6 +99,7 @@ public class App {
         exercise2.largestNumber();
 
         System.out.println("\nTask 2: Stairs");
+        int n = 4;
         exercise2.stairs();
 
         System.out.println("\nTask 3: Pyramide");
